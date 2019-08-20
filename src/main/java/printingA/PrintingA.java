012c73397ac21548e2db97615f87d255f650e17a
Elzbieta Majcher <ela.majcher@gmail.com>
2019-08-20 15:39:36 +0200

    printingNumOfa test passing

--- src/main/java/printingA/PrintingA.java
+++ src/main/java/printingA/PrintingA.java
@@ -5,6 +5,23 @@ public class PrintingA {
 
     static long repeatedString(String s, long n) {
         long numberOfA = 0;
+        char [] input = s.replaceAll("a", "").toCharArray();
+        char [] chars = s.toCharArray();
+        long wordLength = s.length();
+        long aNumInWord = wordLength - input.length;
+
+        long repeat = n / wordLength;
+
+        long rest = n - (repeat * wordLength);
+
+        long plus = 0;
+
+        for (int i = 0; i < rest ; i++) {
+            if (chars[i] == 'a') plus++;
+        }
+
+        numberOfA = (aNumInWord * repeat) + plus;
+
 
         return numberOfA;
     }
