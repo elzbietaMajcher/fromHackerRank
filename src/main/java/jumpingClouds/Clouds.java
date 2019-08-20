cd1772fcb1602ff319cbeba926b629a85549f4c7
Elzbieta Majcher <ela.majcher@gmail.com>
2019-08-20 11:46:07 +0200

    test pass

--- src/main/java/jumpingClouds/Clouds.java
+++ src/main/java/jumpingClouds/Clouds.java
@@ -5,6 +5,17 @@ public class Clouds {
     static int jumpingOnClouds(int[] c) {
         int jumpNum = 0;
 
+        int oneStepBack = 0;
+        int twoStepBack = 0;
+
+
+        for (int cloud: c) {
+            if (cloud == 0 && oneStepBack == 0 && twoStepBack == 1){ jumpNum++; twoStepBack = oneStepBack; oneStepBack = cloud;}
+            else if (cloud == 0 && oneStepBack == 1) {jumpNum++; twoStepBack = oneStepBack; oneStepBack = cloud;}
+            else if (cloud == 1){twoStepBack = oneStepBack; oneStepBack = cloud;}
+
+        }
+
         return jumpNum;
     }
 
--- src/test/java/jumpingClouds/CloudsTest.java
+++ src/test/java/jumpingClouds/CloudsTest.java
@@ -7,6 +7,7 @@ public class CloudsTest {
 
     int [] given1 = new int[]{0, 0, 1, 0, 0, 1, 0}; //min jumps: 3
     int [] given2 = new int[]{0, 1, 0, 0, 0, 1, 0, 1, 0, 0}; //min jumps: 4
+    int [] given3 = new int[] {0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,0}; //min jumps: 9
 
     @org.junit.Test
     public void jumpingOnClouds() {
