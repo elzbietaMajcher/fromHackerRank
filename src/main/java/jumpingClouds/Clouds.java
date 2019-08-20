0968d1de9ee781f9c14a645ef63918ed6f0a9d9c
Elzbieta Majcher <ela.majcher@gmail.com>
2019-08-20 11:17:14 +0200

    test failing

---
+++ src/main/java/jumpingClouds/Clouds.java
@@ -0,0 +1,11 @@
+package jumpingClouds;
+
+public class Clouds {
+
+    static int jumpingOnClouds(int[] c) {
+        int jumpNum = 0;
+
+        return jumpNum;
+    }
+
+}
---
+++ src/test/java/jumpingClouds/CloudsTest.java
@@ -0,0 +1,20 @@
+package jumpingClouds;
+
+import static org.junit.Assert.*;
+
+public class CloudsTest {
+
+
+    int [] given1 = new int[]{0, 0, 1, 0, 0, 1, 0}; //min jumps: 3
+    int [] given2 = new int[]{0, 1, 0, 0, 0, 1, 0, 1, 0, 0}; //min jumps: 4
+
+    @org.junit.Test
+    public void jumpingOnClouds() {
+
+        int actual = Clouds.jumpingOnClouds(given1);
+        int expected = 3;
+        assertEquals(expected, actual);
+
+
+    }
+}
