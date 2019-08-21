5e89ae5572c3527c531ceda146806f7335de8fa4
Elzbieta Majcher <ela.majcher@gmail.com>
2019-08-21 14:58:17 +0200

    PairingSocks passing tests

--- src/main/java/pairingSocks/PairingSocks.java
+++ src/main/java/pairingSocks/PairingSocks.java
@@ -1,14 +1,39 @@
 package pairingSocks;
 
+import java.util.*;
+
 public class PairingSocks {
 
     static int sockMerchant(int n, int[] ar) {
-
         int numOfPair = 0;
-        return numOfPair;
 
+        Map<Integer, Integer> listOfSockColors = segregateToMap(ar);
 
+        Collection <Integer> values = listOfSockColors.values();
+
+        for (int v: values) {
+            if (v>1){
+                int pairs = v/2;
+                numOfPair = numOfPair + pairs;
+            }
+        }
+               return numOfPair;
     }
+
+    public static Map<Integer, Integer> segregateToMap (int [] ar){
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < ar.length; i++) {
+            if (map.containsKey(ar[i])) {
+                map.put(ar[i], map.get(ar[i]) + 1);
+            }else {
+                map.put(ar[i], 1);
+            }
+        }
+        return map;
+    }
+
 }
 
 
--- src/test/java/pairingSocks/PairingSocksTest.java
+++ src/test/java/pairingSocks/PairingSocksTest.java
@@ -2,13 +2,16 @@ package pairingSocks;
 
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class PairingSocksTest {
 
 
     int n = 9;
-    int [] ar = new int[] {10 ,20, 20, 20, 10, 10, 30, 50, 10, 20}; // result, num of pairs: 3
+    int [] ar = {10, 20, 20, 20, 10, 30, 50, 10, 20}; // result, num of pairs: 3
 
     @Test
     public void sockMerchant() {
@@ -17,6 +20,19 @@ public class PairingSocksTest {
 
         assertEquals(3,actual);
 
+
+    }
+
+    @Test
+    public void segregateToMap() {
+
+        Map<Integer, Integer> result  = PairingSocks.segregateToMap(ar);
+
+        Set<Map.Entry<Integer, Integer>> entrySet = result.entrySet();
+        for(Map.Entry<Integer, Integer> entry: entrySet) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+
+        }
 
     }
 }
