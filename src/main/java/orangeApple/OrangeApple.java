d4258393c5ec1af6e732bb0beb82d1e88f655834
Elzbieta Majcher <ela.majcher@gmail.com>
2019-09-02 16:47:12 +0200

    apple orange test

---
+++ src/main/java/orangeApple/OrangeApple.java
@@ -0,0 +1,21 @@
+package orangeApple;
+
+public class OrangeApple {
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int apple = calculate(s, t, a, apples);
+        int orange = calculate(s, t, b, oranges);
+
+        System.out.println(apple);
+        System.out.println(orange);
+    }
+
+    static int calculate(int s, int t, int tree, int [] position){
+        int sum = 0;
+
+
+
+        return sum;
+    }
+
+}
---
+++ src/test/java/orangeApple/OrangeAppleTest.java
@@ -0,0 +1,30 @@
+package orangeApple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrangeAppleTest {
+
+    //GIVEN:
+    int s = 7; int t = 11; int a = 5; int b = 15; int[] apples = new int[]{-2, 2, 1}; int[] oranges = new int[]{5, -6};
+
+    //EXPECTED
+    int appleExp = 1;
+    int orangeExp = 1;
+
+
+    @Test
+    public void countApplesAndOranges() {
+        OrangeApple.countApplesAndOranges(s,t,a,b,apples, oranges);
+    }
+
+    @Test
+    public void calculateTest() {
+
+        int resultApples = OrangeApple.calculate(s,t,a,apples);
+        int resultOranges= OrangeApple.calculate(s,t,b,oranges);
+
+        assertEquals(orangeExp, resultOranges);
+    }
+}
